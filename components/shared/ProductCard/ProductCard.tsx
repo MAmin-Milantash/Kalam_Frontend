226f5586b2be400ea5fcffe3845ae6514040720f
Amin MilanTash <mohammadamin_milantash@yahoo.com>
2026-05-08 22:30:11 +0330

    update product procedure

--- components/shared/ProductCard/ProductCard.tsx
+++ components/shared/ProductCard/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Button from "@/components/ui/Button";
-import {ShoppingCart} from "lucide-react";
+import {Plus, ShoppingCart} from "lucide-react";
+import CustomButton from "@/components/shared/custom-button";
 
 type ProductCardProps = {
     id: string;
@@ -21,10 +21,10 @@ export default function ProductCard({
                                     }: ProductCardProps) {
     return (
         <div className="bg-white flex flex-col items-start justify-start
-                       w-[283px] h-[387px] box-border border border-[#E9E8E3]
+                       max-w-[280px] min-h-[280px] box-border border border-[#E9E8E3]
                        rounded-[20px]   p-[24px_24px_12px_24px] gap-[10px] relative"
              style={{opacity: 1, transform: 'rotate(0deg)'}}>
-            <div className="w-[210px] h-[196px] flex items-center justify-center overflow-hidden rounded-[10px] mx-auto"
+            <div className="md:w-[210px] w-[110px]  flex items-center justify-center overflow-hidden rounded-[10px] mx-auto"
                  style={{opacity: 1, transform: 'rotate(0deg)'}}>
                 <img
                     src={imageUrl}
@@ -36,9 +36,9 @@ export default function ProductCard({
             <div className="w-full h-[1px] bg-[#E9E8E3] my-2" style={{opacity: 1}}></div>
 
             <div className="w-full flex items-center justify-between gap-2">
-                <span className="text-base font-medium text-gray-800">{productName}</span>
+                <span className="md:text-base text-sm font-medium text-gray-800">{productName}</span>
                 <div
-                    className="flex items-center justify-center    w-[61px] h-[31px] box-border
+                    className="flex items-center justify-center min-w-[60px] h-[30px] box-border
                                border border-[#E9E8E3]  rounded-[10px]
                                py-[6px] px-[12px] gap-[10px]" style={{opacity: 1, transform: 'rotate(0deg)'}}>
                     <span className="text-xs font-normal text-gray-600">{unit}</span>
@@ -48,16 +48,17 @@ export default function ProductCard({
             <div className="w-full h-[1px] bg-[#E9E8E3] my-2" style={{opacity: 1}}></div>
 
             <div className="w-full flex items-center justify-between mt-auto">
-                <span className="text-xl font-bold text-gray-900">
+                <span className="md:text-xl text-sm font-bold text-gray-900">
                     {`${price.toLocaleString('fa-IR')} تومان`}
                 </span>
 
-                <Button
-                    label="افزودن به سبد"
+                <CustomButton
+                    label="افزودن"
                     variant="greenOutline"
-                    icon={<ShoppingCart size={18}/>}
+                    icon={<Plus size={18} />}
                     onClick={addToCartHandler}
-                    href={'product/'+id}
+                    href={"product/" + id}
+                    hideLabelOnMobile
                 />
             </div>
         </div>
--- services/product/productService.ts
+++ services/product/productService.ts
@@ -29,6 +29,27 @@ const sampleProducts: Product[] = [
         unit: "کیلو",
         price: 55000,
     },
+    {
+        id: "4",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6e5QA02QniPtNkLNohnGjtfVduFMMWqv4dzRp0XT-Z_mijRrUGruLRAiYWyw1HEG2_8e-s8qQlM9g0mRAAZQNWGcJznFNKSoJg1uV&s=10",
+        productName: "توت فرنگی",
+        unit: "کیلو",
+        price: 120000,
+    },
+    {
+        id: "5",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpvsLi4TWju6HLe8u2kfmk7kfRyLR1kVFR5g&s",
+        productName: "آناناس",
+        unit: "کیلو",
+        price: 98000,
+    },
+    {
+        id: "6",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpBuv9BvfChhah0axEIstyz6Y_-Dtp7hB5PKBLrbZpmX8D58DD2ItZ2e-8uJSbHIUuc_sbJ1HjegvSuyk7xvv-3acqrjMSFSoeE3yJTA&s=10",
+        productName: "آووکادو",
+        unit: "کیلو",
+        price: 55000,
+    },
 ];
 
 // توابع برای دریافت نمونه دیتا
